e278699af330d1ec7541b6024972c045b3c6fd89
Peyton Keimer <pk898422@ohio.edu>
2024-09-03 17:34:44 -0400

    Woop Woop!

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Peyton>
+ *        Date: <09 / 03 / 2024>
+ * Description: The Second CS 2400 Lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
